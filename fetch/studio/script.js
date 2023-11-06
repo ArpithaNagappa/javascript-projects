0a01c7b560eb92cdc5533bd67ade5fe04f45b8a5
Arpitha Nagappa <arpithagn021@gmail.com>
2023-11-05 21:13:01 -0600

    first commit

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,8 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+fetch ("<https://handlers.education.launchcode.org/static/astronauts.json>").then(function(response){
+    response.json().then(function(json){
+    console.log(response);
+    });
+});
+});
